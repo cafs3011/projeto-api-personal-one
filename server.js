31a3bae339847bfdadde6a49ac4c090b7f124045
Pablo Emanuel <vovollice@hotmail.com>
2019-07-14 22:47:02 -0300

    Server.js

--- server.js
+++ server.js
@@ -8,27 +8,14 @@ const usuariosRoute = require("./src/routes/usuario");
 
 const sequelize = require("./src/database/database");
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
-//var passport   = require('passport')
-//var session    = require('express-session')
-=======
 
 var passport   = require('passport');
 var session    = require('express-session');
->>>>>>> d1e19ce71ec19fcd560b7a61950b111150c50979
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 
-<<<<<<< HEAD
-//app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
-//app.use(passport.initialize());
-//app.use(passport.session()); 
-=======
-app.use("/api", alunosRoute);
->>>>>>> d1e19ce71ec19fcd560b7a61950b111150c50979
 app.use("/api", usuariosRoute);
 app.use("/api", alunosRoute);
 
