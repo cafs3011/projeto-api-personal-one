7c785fcfec68a7fa1afe7ed4c770202b86866be0
cafs3011 <cintia.aferreiras@gmail.com>
2019-07-15 22:49:13 -0300

    Alteracao Aluno

--- src/models/aluno.js
+++ src/models/aluno.js
@@ -38,9 +38,7 @@ module.exports =  Aluno.init(
       // This is a reference to another model
       model: usuario,
       // This is the column name of the referenced model
-      key: 'id',
-      // This declares when to check the foreign key constraint. PostgreSQL only.
-      deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      key: 'id'
     }
   },
 },
