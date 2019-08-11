1be4cbf3ca739711eee976b4445fa284e355b233
cafs3011 <cintia.aferreiras@gmail.com>
2019-08-11 18:31:58 -0300

    Alteração banco oracle

--- src/config/config.js
+++ src/config/config.js
@@ -4,12 +4,12 @@ module.exports =
     {
         "database":
         {
-            "host" : "localhost",
+            "host" : "personal-one.cpbyzndbv8yx.us-east-1.rds.amazonaws.com",
             "port" : 3306,
             "name": "personal_one",
             "dialect": "mysql",
             "user": "root",
-            "password": "root"
+            "password": "12345678"
         }
         //
     },
@@ -18,8 +18,12 @@ module.exports =
         "database":
         {
             
-            "host":"process.env.DB_HOST",
-            "port" : "process.env.DB_PORT"
+            "host":"personal-one.cpbyzndbv8yx.us-east-1.rds.amazonaws.com",
+            "port" : 3306,
+            "name": "personal_one",
+            "dialect": "mysql",
+            "user": "root",
+            "password": "12345678"
         }
     }
 };
