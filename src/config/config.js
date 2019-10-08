b5d326169d1aee58ff0c011eff877e958dd20f9a
Pablo Emanuel <vovollice@hotmail.com>
2019-10-07 21:08:20 -0300

    banco

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   development: {
     database: {
-      host: "localhost",
+      host: "locpersonal-one.cpbyzndbv8yx.us-east-1.rds.amazonaws.comalhost",
       port: 3306,
       name: "personal_one",
       dialect: "mysql",
       user: "root",
-      password: "root"
+      password: "12345678"
     }
     //
   },
